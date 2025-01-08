f75844e7237d1000e7cf316bb4e1deef6aa15192
Spikebolt76 <168162977+Spikebolt76@users.noreply.github.com>
2025-01-08 21:31:14 +0530

    lab Commit

--- Lab_01/program_04/src/add_2_nums_Scanner.java
+++ Lab_01/program_04/src/add_2_nums_Scanner.java
@@ -1,4 +1,19 @@
 // Write a program to get 2 numbers from the user and print the sum of two numbers using Scanner class.
 
-public class add_2_nums_Scanner {
+import java.util.Scanner;
+
+public class add_2_nums_Scanner
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter two integers : ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.printf("num1 + num2 = %d", sum);
+    }
 }
