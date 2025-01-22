52bcd59f05109e9d9fc55bb959ba2bf25f36b192
Spikebolt76 <168162977+Spikebolt76@users.noreply.github.com>
2025-01-22 21:04:30 +0530

    lab Commit

--- Lab_05/program_5_03/src/PrintNumFromRotatedArray.java
+++ Lab_05/program_5_03/src/PrintNumFromRotatedArray.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 /*
-There is an integer array nums sorted in ascending order (with distinct values). Prior to being passed to your function,
-nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is
-[nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+There is an integer array num sorted in ascending order (with distinct values). Prior to being passed to your function,
+num is possibly rotated at an unknown pivot index k (1 <= k < num.length) such that the resulting array is
+[num[k], num[k+1], ..., num[n-1], num[0], num[1], ..., num[k-1]] (0-indexed).
 
 For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
 
-Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums,
-or -1 if it is not in nums.
+Given the array num after the possible rotation and an integer target, return the index of target if it is in num,
+or -1 if it is not in num.
 
 Example 1:
-    Input: nums = [4,5,6,7,0,1,2],
+    Input: num = [4,5,6,7,0,1,2],
     target = 0 Output: 4
 Example 2:
-    Input: nums = [4,5,6,7,0,1,2],
+    Input: num = [4,5,6,7,0,1,2],
     target = 3 Output: -1
  */
 
